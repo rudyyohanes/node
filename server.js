a656582cb3268ff78b153dfc428bb24b801b786a
RudyYohanes <rudyyohanesliem@gmail.com>
2021-05-04 06:20:25 +0700

    Add nodemon package.

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+require('dotenv').config();
 
-// Parse application/json
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
+// // Parse application/json
+// app.use(bodyParser.urlencoded({extended: true}));
+// app.use(bodyParser.json());
 
-// Call routes
-var routes = require('./routes');
-routes(app);
+// // Call routes
+// var routes = require('./routes');
+// routes(app);
 
-app.listen(3000, () => {
-    console.log('Server started on port');
+const port = process.env.PORT_APP || 5000;
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
 });
