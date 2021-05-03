097410ad62c8af97ae58aa74b317e50d2a12126e
RudyYohanes <rudyyohanesliem@gmail.com>
2021-05-04 00:00:07 +0700

    Display all user data from database success.

--- controller.js
+++ controller.js
@@ -3,6 +3,17 @@
 var response = require('./res');
 var connection = require('./koneksi');
 
-exports.index = function(req, res){
-    response.ok("This REST API application is working properly.");
+exports.index = function (req, res) {
+    response.ok("This REST API application is working properly.", res);
+};
+
+// View all data from table
+exports.displayall = function (req, res) {
+    connection.query('SELECT * FROM user', function (error, rows, fields) {
+        if (error) {
+            connection.log(error);
+        } else {
+            response.ok(rows, res)
+        }
+    });
 };
--- routes.js
+++ routes.js
@@ -5,4 +5,7 @@ module.exports = function (app) {
 
     app.route('/')
         .get(myjson.index);
-}
+
+    app.route('/view')
+        .get(myjson.displayall);
+};
