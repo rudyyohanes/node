7add26c8de168fe209da98d32374e5e1f3e86dbe
RudyYohanes <rudyyohanesliem@gmail.com>
2021-05-04 08:27:05 +0700

    Fix error and add readme file.

---
+++ README.md
@@ -0,0 +1,9 @@
+Welcome. Here you can see how to use this restful api server on your machine
+
+To run the program, open terminal (linux, macos) or use gitbash for windows
+point it using cd to the /node folder
+
+Type npm start
+
+Open chrome browser
+Type localhost:3000
--- server.js
+++ server.js
@@ -3,15 +3,15 @@ const bodyParser = require('body-parser');
 const app = express();
 require('dotenv').config();
 
-// // Parse application/json
-// app.use(bodyParser.urlencoded({extended: true}));
-// app.use(bodyParser.json());
+// Parse application/json
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
-// // Call routes
-// var routes = require('./routes');
-// routes(app);
+// Call routes
+var routes = require('./routes');
+routes(app);
 
-const port = process.env.PORT_APP || 5000;
+const port = process.env.PORT_APP || 3000;
 
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
